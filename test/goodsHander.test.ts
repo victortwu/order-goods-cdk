ca012ae9cd491d31154fcd7251187b0042a6db48
victor-twu <victor.twu@slalom.com>
2024-01-18 14:36:36 -0800

    forgot to hit save on the test file

--- test/goodsHander.test.ts
+++ test/goodsHander.test.ts
@@ -3,33 +3,20 @@ import { goodsHandler } from "../lib/services/goodsHandler";
 
 process.env.PRODUCTS_TABLE = "ProductsTable-02b5a9df58b3";
 
-test("GET lists", async () => {
-  const event = {
-    httpMethod: "GET",
-    queryStringParameters: {
-      id: "3326d246-5267-4e17-810a-ea981f066fc3",
-    },
-  } as unknown as APIGatewayEvent;
-  const context = {} as Context;
-  const response = await goodsHandler(event, context);
-  console.log(response);
-});
+// test("GET good", async () => {
+//   const event = {
+//     httpMethod: "GET",
+//     queryStringParameters: {
+//       id: "3326d246-5267-4e17-810a-ea981f066fc3",
+//     },
+//   } as unknown as APIGatewayEvent;
+//   const context = {} as Context;
+//   const response = await goodsHandler(event, context);
+//   console.log(response);
+// });
 
-test.skip("POST lists", async () => {
-  const body = {
-    list: [
-      {
-        name: "halal chuck roll",
-        upc: "1234",
-        quantity: { number: 5, caseOrUnit: "case" },
-      },
-      {
-        name: "halal chicken thigh",
-        upc: "5678",
-        quantity: { number: 7, caseOrUnit: "case" },
-      },
-    ],
-  };
+test.skip("POST goods", async () => {
+  const body = {};
   const event = {
     httpMethod: "POST",
     body: JSON.stringify(body),
